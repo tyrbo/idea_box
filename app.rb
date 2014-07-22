0916b0e83373ed2db86d1d1f10b983f762684ab3
Jonmichael Chambers <jon@tyrbo.net>
2014-07-22 09:17:58 -0600

    Implements idea deletion

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    'Hello, world!'
+    erb :index, locals: { ideas: Idea.all }
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
   end
 end
