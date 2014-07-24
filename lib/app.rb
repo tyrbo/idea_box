c624bd34a1b00b2b87a3328691b8727cabc2e90e
Jonmichael Chambers <jon@tyrbo.net>
2014-07-24 15:35:42 -0600

    Fixing stuff

--- lib/app.rb
+++ lib/app.rb
@@ -64,7 +64,7 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/sms' do
-    puts "Got params: #{params}"
+    puts "Got params: #{params}."
     if user = UserStore.find_by_number(params[:From])
       title, description = params[:Body].split(',', 2)
       IdeaStore.create({ 'title' => title, 'description' => description.strip }, user)
